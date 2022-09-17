540b21962ef692955a464c3362113e23adb1ae3d
Kanishak Chaurasia <kanishakchaurasia2@gmail.com>
2022-09-17 13:53:06 +0530

    calander init

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  return (
+  <div>
+    <nav></nav>
+  <Component {...pageProps} />
+  <footer>
+    
+  </footer>
+  </div>
+  )
 }
 
 export default MyApp
